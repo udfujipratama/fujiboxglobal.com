a45c6b17ba62bf8d520f99a112368021ec020fa9
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-21 18:54:37 +0700

    docs: fix name

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>PT Fujibox Global Pratama</h1>
+      <h1>Fujibox - PT Fuji Pratama Global</h1>
     </div>
   )
 }
