7f488d679cc830dfc63d940fd13c7c1395ea6509
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-21 18:18:47 +0700

    style: whatsapp card

--- app/components/whatsapp-card.tsx
+++ app/components/whatsapp-card.tsx
@@ -4,15 +4,22 @@ interface WhatsAppCardProps {}
 
 export const WhatsAppCard: FunctionComponent<WhatsAppCardProps> = () => {
   return (
-    <div className="card card-side bg-green-300 text-green-700 shadow-xl">
+    <div className="card card-side bg-whatsapp text-white shadow-xl max-w-4xl">
       <div className="card-body">
-        <h2 className="card-title">Leave us a message</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
-          incidunt beatae facilis dolor
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-accent">Message us</button>
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="card-title text-3xl mb-2">Leave us a message</h2>
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Consequatur incidunt beatae facilis dolor
+            </p>
+          </div>
+          <div>
+            <button className="btn gap-2 bg-white hover:bg-slate-100 text-whatsapp">
+              Message us
+              <span>(WA)</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
