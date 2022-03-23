5c320d20d5491ba7732829562e1d4a7b6e198b63
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-23 16:46:36 +0700

    feat: whatsapp anchor button

--- app/components/whatsapp-card.tsx
+++ app/components/whatsapp-card.tsx
@@ -1,32 +1,37 @@
-import { FunctionComponent } from 'react';
-import { IoLogoWhatsapp } from 'react-icons/io';
+import { FunctionComponent } from 'react'
+import { IoLogoWhatsapp } from 'react-icons/io'
 
 interface WhatsAppCardProps {}
 
 export const WhatsAppCard: FunctionComponent<WhatsAppCardProps> = () => {
-	return (
-		<div className="container lg mb-8">
-			<div className="card card-side bg-whatsapp text-white shadow-xl ">
-				<div className="card-body">
-					<div className="flex justify-between items-center">
-						<div>
-							<h2 className="card-title text-3xl mb-2">
-								Ingin terhubung langsung dengan marketing kami ?
-							</h2>
-						</div>
-						<div>
-							<button className="btn btn-lg btn-wide gap-2 bg-white hover:bg-slate-100 text-whatsapp">
-								Tanya Fujibox
-								<span>
-									<IoLogoWhatsapp />
-								</span>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className="container lg mb-8">
+      <div className="card card-side bg-whatsapp text-white shadow-xl ">
+        <div className="card-body">
+          <div className="flex justify-between items-center">
+            <div>
+              <h2 className="card-title text-3xl mb-2">
+                Ingin terhubung langsung dengan marketing kami ?
+              </h2>
+            </div>
+            <div>
+              <a
+                href="https://wa.me/6281234567890"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-lg btn-wide gap-2 bg-white hover:bg-slate-100 text-whatsapp"
+              >
+                Tanya Fujibox
+                <span>
+                  <IoLogoWhatsapp />
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default WhatsAppCard;
+export default WhatsAppCard
