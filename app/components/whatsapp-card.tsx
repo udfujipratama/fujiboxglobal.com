b6593728142d420b61a22e301b3a27f11cae76c7
San Sebastian <irsanoncode@gmail.com>
2022-03-23 11:10:15 +0700

    style: fullsize whatsapp card

--- app/components/whatsapp-card.tsx
+++ app/components/whatsapp-card.tsx
@@ -1,30 +1,32 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent } from "react";
 
 interface WhatsAppCardProps {}
 
 export const WhatsAppCard: FunctionComponent<WhatsAppCardProps> = () => {
   return (
-    <div className="card card-side bg-whatsapp text-white shadow-xl max-w-4xl">
-      <div className="card-body">
-        <div className="flex justify-between items-center">
-          <div>
-            <h2 className="card-title text-3xl mb-2">
-              Kontak kami via WhatsApp
-            </h2>
-            <p className="max-w-xl">
-              Memudahkan Anda untuk berkomunikasi dengan tim kami.
-            </p>
-          </div>
-          <div>
-            <button className="btn gap-2 bg-white hover:bg-slate-100 text-whatsapp">
-              WhatsApp Kami
-              <span>(WA)</span>
-            </button>
+    <div className="container lg mb-4">
+      <div className="card card-side bg-whatsapp text-white shadow-xl ">
+        <div className="card-body">
+          <div className="flex justify-between items-center">
+            <div>
+              <h2 className="card-title text-3xl mb-2">
+                Kontak kami via WhatsApp
+              </h2>
+              <p className="max-w-xl">
+                Memudahkan Anda untuk berkomunikasi dengan tim kami.
+              </p>
+            </div>
+            <div>
+              <button className="btn gap-2 bg-white hover:bg-slate-100 text-whatsapp">
+                WhatsApp Kami
+                <span>(WA)</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatsAppCard
+export default WhatsAppCard;
