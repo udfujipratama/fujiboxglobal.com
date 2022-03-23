39cd7310ec5c2dfa37b4e57948433253d7b33fb2
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-23 16:50:04 +0700

    feat: fetch categories from graphcms

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -26,27 +26,20 @@ export const loader: LoaderFunction = async () => {
           url
         }
       }
+      categories {
+        id
+        slug
+        name
+        description
+        image {
+          url
+        }
+      }
     }
   `
 
   const response = await graphcmsClient.query(allProductsQuery).toPromise()
-  const { products } = response.data
-
-  // const products = [
-  //   { id: 1, name: 'Box 1' },
-  //   { id: 2, name: 'Box 2' },
-  //   { id: 3, name: 'Box 3' },
-  //   { id: 4, name: 'Box 4' },
-  // ]
-
-  const categories = [
-    { id: 1, name: 'Dus Kuning Emas' },
-    { id: 2, name: 'Dus Corrugated' },
-    { id: 3, name: 'Dus kraft' },
-    { id: 4, name: 'Dus Motif Tema' },
-    { id: 5, name: 'Dus Cupcake' },
-    { id: 6, name: 'Dus Donut' },
-  ]
+  const { products, categories } = response.data
 
   return {
     products,
