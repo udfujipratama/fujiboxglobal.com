f3c03323603d222151960d79c0bbbae4c045fda3
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-23 14:48:38 +0700

    style: product category

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -8,19 +8,19 @@ interface CategoryProps {
 export const Category: FunctionComponent<CategoryProps> = ({ categories }) => {
   return (
     <div className="container lg mb-4">
-      <div className="hero min-h-hero bg-base-200 py-6 rounded">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="bg-base-200 p-6 rounded flex flex-col xl:flex-row justify-between xl:justify-between">
+        <div className="flex flex-col justify-between">
+          <h1 className="text-4xl font-bold">Contoh kategori produk</h1>
           <img
+            alt="Contoh kategori produk"
             src="https://images.unsplash.com/photo-1633533452148-a9657d2c9a5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=80"
-            className="max-w-2xl rounded"
+            className="max-w-2xl rounded object-cover h-[600px]"
           />
-          <div>
-            <div className="grid gap-4 grid-cols-2">
-              {categories.map((category) => {
-                return <CategoryCards key={category.id} category={category} />
-              })}
-            </div>
-          </div>
+        </div>
+        <div className="flex flex-wrap gap-4">
+          {categories.map((category) => {
+            return <CategoryCard key={category.id} category={category} />
+          })}
         </div>
       </div>
     </div>
@@ -31,9 +31,9 @@ interface CategoryCardsProps {
   category: category
 }
 
-const CategoryCards: FunctionComponent<CategoryCardsProps> = ({ category }) => {
+const CategoryCard: FunctionComponent<CategoryCardsProps> = ({ category }) => {
   return (
-    <div className="card w-60 bg-base-100 shadow-xl image-full">
+    <div className="card bg-base-100 shadow-xl image-full w-full max-w-[15rem]">
       <figure>
         <img
           src="https://images.unsplash.com/photo-1626253934161-08cfea22e968?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1306&q=80"
@@ -46,5 +46,3 @@ const CategoryCards: FunctionComponent<CategoryCardsProps> = ({ category }) => {
     </div>
   )
 }
-
-export default CategoryCards
