fcc806c57ca8c4f54ab9fe21a33a94899d239d86
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-25 15:07:44 +0700

    feat: home hero content

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,24 +1,23 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react'
+import { Link } from 'remix'
 
 interface HeroProps {}
 
 export const Hero: FunctionComponent<HeroProps> = () => {
-	return (
-		<div className="container lg mb-8">
-			<div className="hero min-h-hero bg-[url(https://images.unsplash.com/photo-1595246007497-15e0ed4b8d96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)]">
-				<div className="hero-overlay bg-opacity-60"></div>
-				<div className="hero-content text-center text-neutral-content">
-					<div className="max-w-md">
-						<h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-						<p className="mb-5">
-							Provident cupiditate voluptatem et in. Quaerat
-							fugiat ut assumenda excepturi exercitationem quasi.
-							In deleniti eaque aut repudiandae et a id nisi.
-						</p>
-						<button className="btn btn-primary">Get Started</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className="container lg mb-8">
+      <div className="hero min-h-hero bg-[url(https://images.unsplash.com/photo-1595246007497-15e0ed4b8d96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Selamat datang</h1>
+            <p className="mb-5">Berbagai produk untuk kemasan dan dus.</p>
+            <Link className="btn btn-primary" to="/products">
+              Lihat semua produk
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
