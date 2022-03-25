52e9fced46a8af58c6c42b4967cb10c872c97da0
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-25 17:17:41 +0700

    feat: use nunito font

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -11,15 +12,32 @@ import type { MetaFunction } from 'remix'
 import styles from './styles/app.css'
 import { Layout } from '~/components'
 
-export function links() {
-  return [{ rel: 'stylesheet', href: styles }]
-}
-
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'Fujibox',
   viewport: 'width=device-width,initial-scale=1',
 })
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap',
+    },
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+  ]
+}
 
 export default function App() {
   return (
