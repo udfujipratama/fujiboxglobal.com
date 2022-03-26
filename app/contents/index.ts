dda2d958a70915f43c746ecbdcd23baaa4187761
San Sebastian <irsanoncode@gmail.com>
2022-03-26 16:06:49 +0700

    refactor: product page to products explorer

--- app/contents/index.ts
+++ app/contents/index.ts
@@ -1,1 +1,2 @@
 export * from './recommended-products'
+export * from './products-explorer'
--- app/routes/produk.tsx
+++ app/routes/produk.tsx
@@ -2,6 +2,7 @@ import { json, Link, LoaderFunction, useLoaderData } from 'remix'
 import { gql } from '@urql/core'
 import { CardsList } from '~/components'
 import { graphcmsClient } from '~/lib'
+import { ProductsExplorer } from '~/contents'
 
 export const loader: LoaderFunction = async () => {
   const allProductsCategoriesCollectionsQuery = gql`
@@ -48,69 +49,11 @@ export default function Products() {
 
   return (
     <>
-      <div className="container my-14">
-        <nav className="text-base breadcrumbs mb-5">
-          <ul>
-            <li>
-              <Link to="/">Beranda</Link>
-            </li>
-            <li>
-              <Link to="/produk">Produk</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div id="sidebar-items" className="flex flex-col xl:flex-row">
-          <aside id="sidebar" className="flex flex-col min-w-[250px] gap-10">
-            <div id="sidebar-categories">
-              <h4 className="font-bold mb-4 underline underline-offset-8">
-                Kategori
-              </h4>
-              <ul>
-                {categories.map((category: any) => (
-                  <Link
-                    key={category.id}
-                    to={`/kategori/${category.slug}`}
-                    className="link link-hover"
-                  >
-                    <li>{category.name}</li>
-                  </Link>
-                ))}
-              </ul>
-            </div>
-            <div id="sidebar-collections">
-              <h4 className="font-bold mb-4 underline underline-offset-8">
-                Koleksi
-              </h4>
-              <ul>
-                {collections.map((collection: any) => (
-                  <Link
-                    key={collection.id}
-                    to={`/koleksi/${collection.slug}`}
-                    className="link link-hover"
-                  >
-                    <li>{collection.name}</li>
-                  </Link>
-                ))}
-              </ul>
-            </div>
-          </aside>
-
-          <div id="items-pagination">
-            <CardsList route="produk" items={products} />
-            <div className="flex justify-center mt-10">
-              <div className="btn-group">
-                <button className="btn btn-sm">«</button>
-                <button className="btn btn-sm btn-active">1</button>
-                <button className="btn btn-sm">2</button>
-                <button className="btn btn-sm">3</button>
-                <button className="btn btn-sm">4</button>
-                <button className="btn btn-sm">»</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProductsExplorer
+        products={products}
+        categories={categories}
+        collections={collections}
+      />
     </>
   )
 }
