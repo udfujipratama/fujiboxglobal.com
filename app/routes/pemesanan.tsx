7d66086224489ab7c474946080f3b30c08ac6b7e
San Sebastian <irsanoncode@gmail.com>
2022-03-27 13:33:05 +0700

    feat: add new whatsapp button for pemesanan

--- app/routes/pemesanan.tsx
+++ app/routes/pemesanan.tsx
@@ -1,18 +1,38 @@
+import { IoLogoWhatsapp } from 'react-icons/io'
+
 export default function Pemesanan() {
   return (
     <>
-      <div className="container flex justify-center">
-        <iframe
-          title="Formulir pemesanan"
-          src="https://docs.google.com/forms/d/e/1FAIpQLScSeQraKJFqo8iIxZYo30h0usmJSvGPNfGDGDAi3m7DJHb3JQ/viewform?embedded=true"
-          width="640"
-          height="1400"
-          frameBorder="0"
-          marginHeight={0}
-          marginWidth={0}
-        >
-          Memuat formulir pemesanan...
-        </iframe>
+      <div className="flex flex-col md:flex-row gap-8 max-w-4xl mx-auto">
+        <div className="container flex justify-center">
+          <iframe
+            title="Formulir pemesanan"
+            src="https://docs.google.com/forms/d/e/1FAIpQLScSeQraKJFqo8iIxZYo30h0usmJSvGPNfGDGDAi3m7DJHb3JQ/viewform?embedded=true"
+            width="640"
+            height="1500"
+            frameBorder="0"
+            marginHeight={0}
+            marginWidth={0}
+          >
+            Memuat formulir pemesanan...
+          </iframe>
+        </div>
+        <div className="container border-solid border-2 h-60 p-4 mt-3 flex flex-col justify-around">
+          <h1 className="text-xl text-center">
+            Ingin terhubung langsung dengan kami ?
+          </h1>
+          <a
+            href="https://wa.me/6281291071355"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-lg btn-wide gap-2 bg-whatsapp mx-auto hover:bg-whatsapp"
+          >
+            Tanya Fujibox
+            <span>
+              <IoLogoWhatsapp />
+            </span>
+          </a>
+        </div>
       </div>
     </>
   )
