446fe4ff27622c5a31712c40920f5bd24aa31bcb
San Sebastian <irsanoncode@gmail.com>
2022-03-29 16:31:42 +0700

    feat: user can select all product by collection

---
+++ app/routes/koleksi.$collectionSlug.tsx
@@ -0,0 +1,61 @@
+import { gql } from '@urql/core'
+import { json, LoaderFunction, useLoaderData } from 'remix'
+import { ProductsExplorer } from '~/contents'
+import { graphcmsClient } from '~/lib'
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { collectionSlug } = params
+
+  const oneCollectionBySlugQuery = gql`
+    query oneCollectionBySlug($slug: String!) {
+      collection(where: { slug: $slug }) {
+        id
+        name
+        image {
+          url
+        }
+        products {
+          id
+          name
+          slug
+          material
+          height
+          length
+          width
+          images {
+            url
+          }
+        }
+      }
+      categories {
+        id
+        slug
+        name
+      }
+      collections {
+        id
+        slug
+        name
+      }
+    }
+  `
+  const response = await graphcmsClient
+    .query(oneCollectionBySlugQuery, { slug: collectionSlug })
+    .toPromise()
+  const { collection, categories, collections } = response.data
+
+  return json({ collection, categories, collections })
+}
+
+export default function CategorySlug() {
+  const { collection, categories, collections } = useLoaderData()
+  return (
+    <>
+      <ProductsExplorer
+        products={collection.products}
+        categories={categories}
+        collections={collections}
+      />
+    </>
+  )
+}
