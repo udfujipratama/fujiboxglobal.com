4d14a8ce7ca51f2f07c036cf6a353080bbc1f84f
San Sebastian <irsanoncode@gmail.com>
2022-03-31 13:35:38 +0700

    style: adjust height

--- app/components/instagram-hero.tsx
+++ app/components/instagram-hero.tsx
@@ -7,11 +7,10 @@ export const InstagramHero: FunctionComponent<InstagramCardProps> = () => {
     <div className="container">
       <div className="hero min-h-screen shadow-2xl">
         <div className="hero-content flex-col gap-1">
-          <div className="my-20">
+          <div className="mt-10 mb-10">
             <h1 className="text-4xl text-center md:text-5xl font-bold">
               Kami hadir di Instagram !
             </h1>
-            <div className="w-full"></div>
             <a
               href={`https://www.instagram.com/fujiboxglobal/?hl=en`}
               className="btn btn-block rounded-full gap-4 mt-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
@@ -25,11 +24,11 @@ export const InstagramHero: FunctionComponent<InstagramCardProps> = () => {
             </a>
           </div>
           <iframe
-            className="w-full aspect-[4/3]"
+            className="w-full aspect-[4/3] mb-4"
             title="instagram-card"
             src="https://embedsocial.com/facebook_album/pro_hashtag/bf5e9745e763f521063fc73ea47cb1e1bcc2202c"
             width="1200"
-            height="700"
+            height="1500"
             frameBorder="0"
             marginHeight={0}
             marginWidth={0}
