ed39bd5588a3763e0806b2e63771c743da3bdc4e
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-04 16:46:55 +0700

    feat: sidebar with disclosure

--- app/contents/products-explorer-sidebar.tsx
+++ app/contents/products-explorer-sidebar.tsx
@@ -1,3 +1,4 @@
+import { Disclosure } from '@headlessui/react'
 import { FunctionComponent } from 'react'
 import { IoIosArrowDropdown } from 'react-icons/io'
 import { Link } from 'remix'
@@ -12,6 +13,32 @@ export const ProductsExplorerSidebar: FunctionComponent<
 > = ({ categories, collections }) => {
   return (
     <aside id="sidebar" className="flex flex-col min-w-[300px] gap-10">
+      <Disclosure>
+        {({ open }) => (
+          <div>
+            <Disclosure.Button className="flex justify-between w-full underline underline-offset-8 font-bold hover:bg-primary-200 focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75">
+              <span>Kategori ({categories.length})</span>
+              <IoIosArrowDropdown
+                className={`${open ? 'transform rotate-180' : ''} w-5 h-5`}
+              />
+            </Disclosure.Button>
+            <Disclosure.Panel className="py-4 text-sm text-gray-900">
+              <ul className="list-none">
+                {categories.map((category: any) => (
+                  <Link
+                    key={category.id}
+                    to={`/kategori/${category.slug}`}
+                    className="link link-hover"
+                  >
+                    <li className="mb-2">{category.name}</li>
+                  </Link>
+                ))}
+              </ul>
+            </Disclosure.Panel>
+          </div>
+        )}
+      </Disclosure>
+
       <div id="sidebar-categories" className="dropdown">
         <div className="flex gap-4">
           <h4
@@ -36,6 +63,7 @@ export const ProductsExplorerSidebar: FunctionComponent<
           ))}
         </ul>
       </div>
+
       <div id="sidebar-collections" className="dropdown mb-10">
         <div className="flex gap-4">
           <h4
