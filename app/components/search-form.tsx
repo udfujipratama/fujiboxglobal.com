fd7d2096e79ff8123a46e82f5b93d7bbfb9abbfc
San Sebastian <irsanoncode@gmail.com>
2022-04-04 22:50:28 +0700

    style: adjust svg search icon size

--- app/components/search-form.tsx
+++ app/components/search-form.tsx
@@ -53,7 +53,7 @@ export const SearchForm: FunctionComponent<SearchFormProps> = () => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-4 w-4"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
