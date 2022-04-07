af6f79553ddebe61f7e0f27a8f71d7f12a832f4c
San Sebastian <irsanoncode@gmail.com>
2022-04-07 15:12:06 +0700

    feat: use remix seo balavishnuvj handle sitemap

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,22 +1,30 @@
-import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
+import { renderToString } from 'react-dom/server'
+import { RemixServer } from 'remix'
 
-import type { EntryContext } from "remix";
+import { otherRootRouteHandlers } from './other-root-routes.server'
 
-export default function handleRequest(
+import type { EntryContext } from 'remix'
+
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const handler of otherRootRouteHandlers) {
+    // eslint-disable-next-line no-await-in-loop
+    const otherRouteResponse = await handler(request, remixContext)
+    if (otherRouteResponse) return otherRouteResponse
+  }
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
-  );
+  )
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html')
 
-  return new Response(`<!DOCTYPE html>${  markup}`, {
+  return new Response(`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
     headers: responseHeaders,
-  });
+  })
 }
---
+++ app/other-root-routes.server.ts
@@ -0,0 +1,24 @@
+import { generateSitemap } from '@balavishnuvj/remix-seo'
+import { EntryContext } from 'remix'
+
+type Handler = (
+  request: Request,
+  remixContext: EntryContext
+) => Promise<Response | null> | null
+
+export const otherRootRoutes: Record<string, Handler> = {
+  '/sitemap.xml': async (request, remixContext) => {
+    return generateSitemap(request, remixContext, {
+      siteUrl: 'https://fujiboxglobal.com',
+    })
+  },
+}
+
+export const otherRootRouteHandlers: Array<Handler> = [
+  ...Object.entries(otherRootRoutes).map(([path, handler]) => {
+    return (request: Request, remixContext: EntryContext) => {
+      if (new URL(request.url).pathname !== path) return null
+      return handler(request, remixContext)
+    }
+  }),
+]
