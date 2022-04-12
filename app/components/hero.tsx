1fc25f1996ee7488e8760c1ae6eb4daf397cf43b
San Sebastian <irsanoncode@gmail.com>
2022-04-12 22:32:33 +0700

    fix: remove w-1280 and unused code

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -9,21 +9,11 @@ export const Hero: FunctionComponent<HeroProps> = () => {
       <div className="hero">
         <Link to="/koleksi/idul-fitri-or-lebaran">
           <img
-            className="w-[1280px] object-cover rounded-xl"
+            className="object-cover rounded-xl"
             src="https://media.graphassets.com/kGDjy6KARruA0MeAFoaH?_gl=1*1f23h42*_ga*OTYxMjM0NjA0LjE2NDc4NDkwNDU.*_ga_G6FYGSYGZ4*MTY0OTA0NzcwNi4xMS4xLjE2NDkwNDc3MjIuNDQ."
             alt="hero-cover"
           />
         </Link>
-        {/* <div className="hero-overlay bg-opacity-60"></div> */}
-        {/* <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Selamat datang</h1>
-            <p className="mb-5">Berbagai produk untuk kemasan dan dus.</p>
-            <Link className="btn btn-primary" to="/produk">
-              Lihat semua produk
-            </Link>
-          </div>
-        </div> */}
       </div>
     </div>
   )
