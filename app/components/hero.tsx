f75624dcbc07e17b5e19fde256942c24f261906a
San Sebastian <irsanoncode@gmail.com>
2022-04-19 10:53:14 +0700

    fix: route to /produk

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,7 +7,7 @@ export const Hero: FunctionComponent<HeroProps> = () => {
   return (
     <div className="container lg mb-8">
       <div className="hero">
-        <Link to="/koleksi/idul-fitri-or-lebaran">
+        <Link to="/produk?koleksi=idul-fitri-or-lebaran">
           <img
             className="object-cover rounded-xl"
             src="https://media.graphassets.com/kGDjy6KARruA0MeAFoaH?_gl=1*1f23h42*_ga*OTYxMjM0NjA0LjE2NDc4NDkwNDU.*_ga_G6FYGSYGZ4*MTY0OTA0NzcwNi4xMS4xLjE2NDkwNDc3MjIuNDQ."
