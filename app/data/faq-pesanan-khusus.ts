b34d1727bb91723a460f11c444566295fc2da112
San Sebastian <irsanoncode@gmail.com>
2022-04-25 12:44:05 +0700

    feat: data fo faq pesanan khusus

---
+++ app/data/faq-pesanan-khusus.ts
@@ -0,0 +1,16 @@
+export const faqPesananKhusus = [
+  {
+    title: 'Minimum order untuk barang ini ?',
+    description: 'Minimal pembelian untuk produk dibawah ini adalah 1000pcs',
+  },
+  {
+    title: 'Apakah ini ready stock ?',
+    description:
+      'Barang ini belom ready stock, akan di produksi dalam waktu 1 bulan',
+  },
+  {
+    title: 'Bagaimana proses pemesanannya ?',
+    description:
+      'Silahkan di pilih produk yang diinginkan lalu, regiterasi ke form yang tersedia',
+  },
+]
