7dee487b65be48b5414324b0a395228b6b6d8775
San Sebastian <irsanoncode@gmail.com>
2022-04-25 12:33:31 +0700

    refactor: comment out unused link menu

--- app/data/navigation-links.ts
+++ app/data/navigation-links.ts
@@ -2,5 +2,5 @@ export const navigationLinks = [
   { title: 'Beranda', link: '/' },
   { title: 'Produk', link: '/produk' },
   { title: 'Pemesanan', link: '/pemesanan' },
-  { title: 'Pesanan Khusus', link: '/pesanan-khusus' },
+  // { title: 'Pesanan Khusus', link: '/pesanan-khusus' },
 ]
