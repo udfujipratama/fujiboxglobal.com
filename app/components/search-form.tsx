46747fa7943110e255a076280a236bb34014745d
San Sebastian <irsanoncode@gmail.com>
2022-05-09 17:22:28 +0700

    feat: changing path svg to react icon

--- app/components/search-form.tsx
+++ app/components/search-form.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { FiSearch } from 'react-icons/fi'
 import { useLocation, useNavigate, useSearchParams } from 'remix'
 
 interface SearchFormProps {}
@@ -40,33 +41,20 @@ export const SearchForm: FunctionComponent<SearchFormProps> = () => {
 
   return (
     <form className="form-control" onSubmit={handleSubmit(onSubmit)}>
-      <div className="input-group">
+      <div className="relative">
         <input
           {...register('searchQuery')}
+          className=" md:w-[500px] md:h-[49px] py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
           defaultValue={searchQuery}
           name="searchQuery"
           type="text"
-          placeholder="Search…"
-          className="input input-xs md:w-[500px] md:h-[49px] input-bordered rounded"
+          placeholder="Search"
         />
         <button
           type="submit"
-          className="btn btn-xs md:btn-md btn-primary btn-square"
+          className="absolute p-2 text-black -translate-y-1/2 rounded-full top-1/2 right-4"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
+          <FiSearch />
         </button>
       </div>
     </form>
