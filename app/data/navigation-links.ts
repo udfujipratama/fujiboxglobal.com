d2a2b31cd1c1da191c3481f50604f8ec6a1475a3
San Sebastian <irsanoncode@gmail.com>
2022-05-11 10:55:56 +0700

    fix: comment out route pemesanan

--- app/data/navigation-links.ts
+++ app/data/navigation-links.ts
@@ -1,6 +1,6 @@
 export const navigationLinks = [
   { title: 'Beranda', link: '/' },
   { title: 'Produk', link: '/produk' },
-  { title: 'Pemesanan', link: '/pemesanan' },
+  // { title: 'Pemesanan', link: '/pemesanan' },
   // { title: 'Pesanan Khusus', link: '/pesanan-khusus' },
 ]
