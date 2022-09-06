3ba98faa38dbcb08dda6bc8190a142ef8998a3d0
San Sebastian <irsanoncode@gmail.com>
2022-09-06 21:45:37 +0700

    style: navbar pesanan khusus mobile view

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -112,7 +112,7 @@ const MainNavbar = () => {
 
 const PesananKhususNavbar = () => {
   return (
-    <div className="flex flex-wrap items-center w-full h-24 sticky top-0 z-50 bg-white sm:px-6 lg:px-12">
+    <div className="flex flex-wrap items-center justify-between w-full h-24 sticky top-0 z-50 bg-white sm:px-6 lg:px-12">
       <div className="flex items-center">
         <Link to="/" className="pl-4 lg:pl-0">
           <img
@@ -123,7 +123,7 @@ const PesananKhususNavbar = () => {
         </Link>
       </div>
 
-      <div className="flex items-center justify-end flex-1 gap-4">
+      <div className="hidden lg:flex items-center justify-end flex-1 gap-4">
         <Link
           to="/"
           className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
@@ -142,6 +142,51 @@ const PesananKhususNavbar = () => {
         >
           Penawaran kami
         </Link>
+      </div>
+
+      <div className="flex items-center border-gray-100 divide-x divide-gray-100 border-x">
+        <span className="lg:hidden">
+          <div className="dropdown dropdown-end">
+            <label
+              tabIndex={0}
+              className="block p-4 lg:p-6 border-b-4 border-transparent hover:border-primary"
+              onMouseDown={(e) => {
+                checkAndCloseDropDown(e)
+              }}
+            >
+              <FiMenu />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+            >
+              <li>
+                <Link
+                  to="/"
+                  className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
+                >
+                  Kembali ke Beranda
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/pesanan-khusus"
+                  className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
+                >
+                  Pesanan Khusus
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/pesanan-khusus-harga"
+                  className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
+                >
+                  Penawaran kami
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </span>
       </div>
     </div>
   )
