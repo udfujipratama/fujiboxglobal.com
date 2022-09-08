7d6670e59dd3b4f46a4e1c4a76e9a2c66241f553
San Sebastian <irsanoncode@gmail.com>
2022-09-08 11:48:06 +0700

    style: padding pesanan khusus hero

--- app/contents/pesanan-khusus/pesanan-khusus-hero.tsx
+++ app/contents/pesanan-khusus/pesanan-khusus-hero.tsx
@@ -6,7 +6,7 @@ export const PesananKhususHero: FunctionComponent<
   PesananKhususHeroProps
 > = () => {
   return (
-    <div className="px-4 py-0 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="px-4 py-0 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-9">
       <div className="flex flex-col items-center justify-between lg:flex-row">
         <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
           <div className="max-w-xl mb-6">
