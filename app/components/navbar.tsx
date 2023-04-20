7eaff64e4e9f7442cb46c566780bc0fd2c840534
sanBastia <irsanoncode@gmail.com>
2023-04-20 14:21:32 +0700

    refactor: nav for mobile and desktop

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,11 +4,22 @@ import { Link } from '@remix-run/react'
 import { Fragment } from 'react'
 import { IoMenuSharp } from 'react-icons/io5'
 
-export const Navbar = () => {
-  return PesananKhususNavbar()
-}
+const navigationLinks = [{
+  title: 'Beranda',
+  link: '/'
+},
+// {
+//   title: 'katalog',
+//   link: '/katalog-produk'
+// },
+{
+  title: 'penawaran',
+  link: '/pesanan-khusus-penawaran'
+}]
 
-const PesananKhususNavbar = () => {
+
+
+export const Navbar = () => {
   return (
     <div className="flex flex-wrap items-center justify-between max-w-8xl mx-auto h-24 sticky top-0 z-50 bg-white sm:px-6 lg:px-12">
       <div className="flex items-center">
@@ -20,28 +31,22 @@ const PesananKhususNavbar = () => {
           />
         </Link>
       </div>
-
+{/* desktop navigation */}
       <div className="hidden lg:flex items-center justify-end flex-1 gap-4">
-        <Link
-          to="/"
-          className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
-        >
-          Beranda
-        </Link>
-        {/* <Link
-          to="/katalog-produk"
-          className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
-        >
-          Katalog
-        </Link> */}
-        <Link
-          to="/pesanan-khusus-penawaran"
-          className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
-        >
-          Penawaran
-        </Link>
+        {navigationLinks.map((item)=>{
+          return (
+            <Link
+            key={item.title}
+            to={item.link}
+            className="h-16 leading-[4rem] border-b-4 border-transparent hover:text-primary hover:border-primary"
+          >
+            {item.title}
+          </Link>
+          )
+        })}
       </div>
 
+{/* mobile navigation */}
       <div className="flex items-center border-gray-100 divide-x divide-gray-100 border-x">
         <span className="lg:hidden">
           <Menu as="div" className="relative inline-block text-left">
@@ -61,51 +66,27 @@ const PesananKhususNavbar = () => {
               leaveTo="transform opacity-0 scale-95"
             >
               <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      // eslint-disable-next-line arrow-body-style
-                      <Link
-                        to="/"
-                        className={`${
-                          active ? 'bg-fujibox text-white' : 'text-gray-900'
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        Beranda
-                      </Link>
-                    )}
-                  </Menu.Item>
+              {navigationLinks.map((item)=>{
+                    return (
+                      <div
+                      key={item.title}
+                       className="px-1 py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              // eslint-disable-next-line arrow-body-style
+                              <Link
+                                to={item.link}
+                                className={`${
+                                  active ? 'bg-fujibox text-white' : 'text-gray-900'
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                {item.title}
+                              </Link>
+                            )}
+                          </Menu.Item>
                 </div>
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      // eslint-disable-next-line arrow-body-style
-                      <Link
-                        to="/katalog-produk"
-                        className={`${
-                          active ? 'bg-fujibox text-white' : 'text-gray-900'
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        Katalog
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      // eslint-disable-next-line arrow-body-style
-                      <Link
-                        to="/pesanan-khusus-penawaran"
-                        className={`${
-                          active ? 'bg-fujibox text-white' : 'text-gray-900'
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        Penawaran
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
+                    )
+                  })}
               </Menu.Items>
             </Transition>
           </Menu>
@@ -114,3 +95,5 @@ const PesananKhususNavbar = () => {
     </div>
   )
 }
+
+
